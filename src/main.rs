a8d913021f7a1b3095d5b621801d5752f36fcfcc
vkazbanov <vkazbanov@telematika.com>
2026-02-28 14:24:08 +0200

    Now everything actually works

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,30 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-extern crate stm32f4xx_hal as hal;
-extern crate cortex_m_rt as rt;
-extern crate panic_semihosting;
+use panic_halt as _;
 
-use hal::gpio::{GpioExt, Pin};
-use hal::prelude::*;
+use stm32f4xx_hal as hal;
 
-#[rt::entry]
+use crate::hal::{pac, prelude::*};
+use cortex_m_rt::entry;
+
+#[entry]
 fn main() -> ! {
     let core = cortex_m::Peripherals::take().unwrap();
-    let peripherals = hal::pac::Peripherals::take().unwrap();
+    let p = pac::Peripherals::take().unwrap();
 
-    let fpu_cpacr = peripherals.FPU_CPACR;
-    fpu_cpacr.cpacr.write(|w| unsafe { w.cp().bits(0b1111) });
-    let rcc = peripherals.RCC;
-    rcc.cr.modify(|r, w|
-        w
-            .hsion().set_bit()
-            .hseon().clear_bit()
-            .csson().clear_bit()
-            .pllon().clear_bit()
-    );
-    rcc.pllcfgr.modify(|r, w|
-        unsafe {
-            w
-                .bits(0b100100000000000011000000010000)
 
-        }
-    );
-    rcc.cr.modify(|r, w|
-        w
-            .hsebyp().clear_bit()
-    );
-    rcc.cir.modify(|r, w|
-        unsafe {
-            w.bits(0)
-        }
-    );
-
-    let rcc = rcc.constrain();
+    let rcc = p.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
-    let bus_c = peripherals.GPIOC.split();
-
-    let mut led: Pin<'C', 13, _> = bus_c.pc13.into_push_pull_output();
+    let gpioc = p.GPIOC.split();
+    let mut led = gpioc.pc13.into_push_pull_output();
 
     let mut delay = core.SYST.delay(&clocks);
 
     loop {
         led.set_low();
-        // delay.delay_ms(100u32);
+        delay.delay_ms(100u32);
         led.set_high();
         delay.delay_ms(1000u32);
     }
