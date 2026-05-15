2ef4979aba4f6b410fa8ef12030c3abb0b9683ff
Radviger <radviger@gmail.com>
2026-05-15 22:29:13 +0200

    Working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write;
 use {defmt_rtt as _, panic_probe as _};
 
 use stm32f4xx_hal as hal;
 
 use crate::hal::{pac, prelude::*};
 use cortex_m_rt::entry;
+use ssd1306::{I2CDisplayInterface, Ssd1306};
+use ssd1306::prelude::*;
 use stm32f4xx_hal::block;
+use stm32f4xx_hal::i2c::Mode;
 use stm32f4xx_hal::serial::Config;
 
 #[entry]
@@ -20,8 +22,9 @@ fn main() -> ! {
     let rcc = p.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
-    let gpioc = p.GPIOC.split();
+    let gpioa = p.GPIOA.split();
     let gpiob = p.GPIOB.split();
+    let gpioc = p.GPIOC.split();
     let mut led = gpioc.pc13.into_push_pull_output();
 
     let mut delay = core.SYST.delay(&clocks);
@@ -31,37 +34,48 @@ fn main() -> ! {
 
     let mut serial = p.USART1.serial::<_, _, u8>((tx, rx), Config::default(), &clocks).unwrap();
 
-    let mut array: [u8; 256] = [0; 256];
-    let mut len = 0;
+    let scl = gpiob.pb8;
+    let sda = gpiob.pb9;
+
+    let i2c = p.I2C1.i2c((scl, sda), Mode::Standard {
+        frequency: 100.kHz(),
+    }, &clocks);
+
+    let interface = I2CDisplayInterface::new(i2c);
+
+    let mut display = Ssd1306::new(
+        interface,
+        DisplaySize96x16,
+        DisplayRotation::Rotate0,
+    ).into_terminal_mode();
+
+    display.clear().unwrap();
+    display.init().unwrap();
+
+    let mut row = 0;
+    let mut column = 0;
 
     loop {
         let byte = block!(serial.read()).unwrap();
-        let _ = block!(serial.write(byte));
-        if byte == b'\x7f' { //Backspace
-            if len > 0 {
-                len -= 1;
-            }
-        } else if byte == b'\r' {
-            let s = str::from_utf8(&array[0..len]).unwrap();
-            len = 0;
-            serial.write(b'\r').unwrap(); // New line
-            serial.write(b'\n').unwrap(); // New line
+        let c = byte as char;
+        defmt::info!("Received character: {}", c);
 
-            if s == "led on" {
-                led.set_low();
-                serial.write_str("Turned on LED\n").unwrap();
-            } else if s == "led off" {
-                led.set_high();
-                serial.write_str("Turned off LED\n").unwrap();
+        if c.is_ascii_alphanumeric() || c.is_ascii_punctuation() || c == ' ' {
+            column += 1;
+            display.print_char(c).unwrap();
+        } else if c == '\r' {
+            row += 1;
+            if row >= 2 {
+                row = 0;
             }
-        } else if byte == b'\n' {
-            // defmt::info!("New line: {:?}", &array[0..len]);
-        } else {
-            let c = byte as char;
-            defmt::info!("Received character: {:?}", c);
-            array[len] = byte;
-            len += 1;
+            display.set_row(row * 8).unwrap();
+            display.set_column(0).unwrap();
+        } else if c == '\x7f' {
+            display.set_column(column).unwrap();
+            display.print_char(' ').unwrap();
+            if column > 0 {
+                column -= 1;
+            }
         }
-
     }
 }
